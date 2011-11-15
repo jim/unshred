12a285c111629c511ad4469df6a3e608b1cc1092
Jim Benton <jim@autonomousmachine.com>
2011-11-14 22:12:21 -0600

    minor refactor

--- lib/unshred/strip.rb
+++ lib/unshred/strip.rb
@@ -24,14 +24,18 @@ module Unshred
     def compute_match(lcol, rcol)
       differences = lcol.each_with_index.map do |lval, index|
         right_pixel = rcol[index]
-        r = Color.r(lval) - Color.r(right_pixel)
-        g = Color.g(lval) - Color.g(right_pixel)
-        b = Color.b(lval) - Color.b(right_pixel)
-
-        [r,g,b].inject {|sum,n| sum + n.abs }
+        pixel_difference(lval, right_pixel)
       end
 
       differences.inject {|sum,n| sum + n} / lcol.size.to_f
+    end
+
+    def pixel_difference(left_pixel, right_pixel)
+      r = Color.r(left_pixel) - Color.r(right_pixel)
+      g = Color.g(left_pixel) - Color.g(right_pixel)
+      b = Color.b(left_pixel) - Color.b(right_pixel)
+
+      [r,g,b].inject {|sum,n| sum + n.abs }
     end
   end
 end
